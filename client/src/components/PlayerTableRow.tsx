8d82b92130610c0bc404f97c3bc808cf03644335
steve bai <baiengg206@gmail.com>
2022-10-17 17:29:34 -0400

    Create PlayerListTable

---
+++ client/src/components/PlayerTableRow.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { PlayerBriefData } from "./PlayerListTable";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import { getClubSrc, getPlayerSrc } from "../utils";
+
+const PlayerTableRow = ({playerData}: { playerData: PlayerBriefData }) => (
+  <TableRow
+    key={playerData.sofifa_id}
+    sx={{"&:last-child td, &:last-child th": {border: 0}}}
+  >
+    <TableCell component="th" scope="row">
+      <span><img src={getPlayerSrc(playerData.sofifa_id)} alt={playerData.short_name} /></span>
+      {playerData.short_name}
+    </TableCell>
+    <TableCell align="right">{playerData.age}</TableCell>
+    <TableCell align="right">{playerData.player_positions}</TableCell>
+    <TableCell align="right">{playerData.overall}</TableCell>
+    <TableCell align="right">{playerData.potential}</TableCell>
+    <TableCell align="center">
+      <span><img src={getClubSrc(playerData.clubs.ID)} alt={playerData.clubs.Name} /></span>
+      {playerData.clubs.Name}
+    </TableCell>
+  </TableRow>
+);
+
+export default PlayerTableRow;
---
+++ client/src/hooks/usePlayers.ts
@@ -0,0 +1,27 @@
+import { useQuery, gql } from "@apollo/client";
+
+const GET_PLAYERS = gql`
+ query {
+  players ( options: { limit: 100 } ) {
+    sofifa_id,
+    short_name,
+    player_positions,
+    age,
+    overall,
+    potential,
+    clubs {
+      ID
+      Name
+    }
+  }
+ }
+`
+
+export const usePlayers = () => {
+  const { data, loading } = useQuery(GET_PLAYERS);
+
+  return {
+    data,
+    loading
+  };
+};
