ba44518313d0eee578adb45eaae347f50d5cdd67
steve bai <baiengg206@gmail.com>
2022-10-30 17:54:47 -0400

    Refactor PlayerListTable

--- client/src/components/PlayerListTable/PlayerListTable.tsx
+++ client/src/components/PlayerListTable/PlayerListTable.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
 import { usePlayers } from "../../hooks/usePlayers";
-import { tableCategories } from "../../utils";
-import PlayerTableRow from "../PlayerTableRow/PlayerTableRow";
-import { Paper } from "@mui/material";
+import Players from "../Players/Players";
 
 interface Club {
   ID: number;
@@ -28,33 +20,12 @@ export interface PlayerBriefData {
 const PlayersListTable = () => {
   const {loading, data} = usePlayers();
 
-  const tableHead = tableCategories.map((tableCategories) => {
-    return <TableCell key={tableCategories.label} align="right">{tableCategories.label}</TableCell>;
-  });
-
   if (loading) {
     return <div>Loading...</div>
   }
 
-  const tableRows = data.players.map((playerData: PlayerBriefData) => (
-    <PlayerTableRow key={playerData.sofifa_id} playerData={playerData} />
-  ));
-
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{minWidth: 650}} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            {tableHead}
-            <TableCell align="center">Team</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {tableRows}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Players playersData={data.players} />
   );
 };
 
